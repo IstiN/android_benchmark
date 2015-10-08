4cd5455e7eb72c483ed2b48b22d395e6a7319c50
egor makovsky <Yahor_Makouski@EPBYMINW4917.minsk.epam.com>
2015-10-08 13:22:30 +0300

    ability to create a custom report with tests result

--- app/src/main/java/com/epam/android/benchmark/Member.java
+++ app/src/main/java/com/epam/android/benchmark/Member.java
@@ -2,9 +2,8 @@ package com.epam.android.benchmark;
 
 import com.epam.benchmark.IMember;
 import com.epam.benchmark.impl.ParserStorageMember;
-import com.epam.benchmark.impl.storage.InMemoryStorage;
 import com.epam.benchmark.impl.storage.SQLiteStorage;
-import com.epam.benchmark.moshi.MoshiParser;
+import com.epam.benchmark.jackson.JacksonParser;
 import com.epam.greendao.GreenDAO;
 import com.epam.ormlite.ORMLite;
 import com.epam.realmio.RealmIo;
@@ -22,77 +21,77 @@ public enum Member {
         }
     },
 
-    MOSHI_IN_MEMORY {
-        @Override
-        public IMember create() {
-            return new ParserStorageMember(new MoshiParser(), new InMemoryStorage());
-        }
-    },
-
-    MOSHI_SIMPLE_SQLITE {
-        @Override
-        public IMember create() {
-            return new ParserStorageMember(new MoshiParser(), new SQLiteStorage());
-        }
-    },
-
-    MOSHI_REALM {
-        @Override
-        public IMember create() {
-            return new ParserStorageMember(new MoshiParser(), new RealmIo());
-        }
-    },
-
-    MOSHI_GREENDAO {
-        @Override
-        public IMember create() {
-            return new ParserStorageMember(new MoshiParser(), new GreenDAO());
-        }
-    },
-
-    MOSHI_ORMLITE {
-        @Override
-        public IMember create() {
-            return new ParserStorageMember(new MoshiParser(), new ORMLite());
-        }
-    },
+//    MOSHI_IN_MEMORY {
+//        @Override
+//        public IMember create() {
+//            return new ParserStorageMember(new MoshiParser(), new InMemoryStorage());
+//        }
+//    },
 //
+//    MOSHI_SQLITE {
+//        @Override
+//        public IMember create() {
+//            return new ParserStorageMember(new MoshiParser(), new SQLiteStorage());
+//        }
+//    },
+//
+//    MOSHI_REALM {
+//        @Override
+//        public IMember create() {
+//            return new ParserStorageMember(new MoshiParser(), new RealmIo());
+//        }
+//    },
+//
+//    MOSHI_GREENDAO {
+//        @Override
+//        public IMember create() {
+//            return new ParserStorageMember(new MoshiParser(), new GreenDAO());
+//        }
+//    },
+//
+//    MOSHI_ORMLITE {
+//        @Override
+//        public IMember create() {
+//            return new ParserStorageMember(new MoshiParser(), new ORMLite());
+//        }
+//    },
+
 //    JACKSON_IN_MEMORY {
 //        @Override
 //        public IMember create() {
 //            return new ParserStorageMember(new JacksonParser(), new InMemoryStorage());
 //        }
 //    },
-//
-//    JACKSON_SQLITE {
-//        @Override
-//        public IMember create() {
-//            return new ParserStorageMember(new JacksonParser(), new SQLiteStorage());
-//        }
-//    },
-//
-//    JACKSON_REALM {
-//        @Override
-//        public IMember create() {
-//            return new ParserStorageMember(new JacksonParser(), new RealmIo());
-//        }
-//    },
-//
-//    JACKSON_GREENDAO {
-//        @Override
-//        public IMember create() {
-//            return new ParserStorageMember(new JacksonParser(), new GreenDAO());
-//        }
-//    },
-//
-//    JACKSON_REALM {
-//        @Override
-//        public IMember create() {
-//            return new ParserStorageMember(new JacksonParser(), new RealmIo());
-//        }
-//    }
-    ;
 
+    JACKSON_SQLITE {
+        @Override
+        public IMember create() {
+            return new ParserStorageMember(new JacksonParser(), new SQLiteStorage());
+        }
+    },
+
+    JACKSON_REALM {
+        @Override
+        public IMember create() {
+            return new ParserStorageMember(new JacksonParser(), new RealmIo());
+        }
+    },
+
+    JACKSON_GREENDAO {
+        @Override
+        public IMember create() {
+            return new ParserStorageMember(new JacksonParser(), new GreenDAO());
+        }
+    },
+
+    JACKSON_ORMLITE {
+        @Override
+        public IMember create() {
+            return new ParserStorageMember(new JacksonParser(), new ORMLite());
+        }
+    }
+
+    ;
 
     public abstract IMember create();
 }
